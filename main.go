a8bedf2ad30d249fb38f6a55c4a5a7d8eb789577
cskksc <ckoparkar@live.in>
2015-04-03 14:52:54 +0530

    GET /v2/catalog returns JSON catalog.

--- main.go
+++ main.go
@@ -12,9 +12,18 @@ func helloWorld(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
+func catalog(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	data, err := Asset("config/settings.json")
+	if err != nil {
+		return
+	}
+	fmt.Fprintf(w, string(data))
+}
+
 func Routes() *httprouter.Router {
 	router := httprouter.New()
 	router.GET("/", helloWorld)
+	router.GET("/v2/catalog", catalog)
 	return router
 }
 
--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -17,5 +19,30 @@ func TestHelloWorld(t *testing.T) {
 
 	if res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode)
+	}
+}
+
+func TestCatalog(t *testing.T) {
+	server := httptest.NewServer(Routes())
+	defer server.Close()
+
+	res, err := http.Get(server.URL + "/v2/catalog")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Error(res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	content := string(body)
+	if !strings.Contains(content, "postgresql-db") {
+		t.Error(err)
 	}
 }
